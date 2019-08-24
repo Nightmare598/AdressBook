b63a00cd43793f3e0f1587deea4c9b5cd30f5164
Samuelcc12 <samuel.cabrera.19999@gmail.com>
2019-08-24 11:01:59 -0400

    Solucion Final

---
+++ MVCAddress/MVCAddress/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MVCAddress.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCAddress.Models.Book> Books { get; set; }
+    }
+}

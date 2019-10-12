c66e2bce9ad4090e401637788c17d88c0469369f
juampi630@gmail.com <juampi630@gmail.com>
2019-10-12 11:06:58 -0400

    MVVM finalizado

---
+++ MVCAddress/appAddress/appAddress/Views/BookPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appAddress.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public BookPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
